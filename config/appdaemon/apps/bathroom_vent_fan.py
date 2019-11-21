e9f13ec077adb817a7258269a86f0d69e07c7201
Zack Lalanne <zack.lalanne@gmail.com>
2019-11-21 07:03:59 -0600

    Add AppDaemon automation for running bathroom vent fan

--- config/appdaemon/apps/bathroom_vent_fan.py
+++ config/appdaemon/apps/bathroom_vent_fan.py
@@ -2,51 +2,72 @@ import appdaemon.plugins.hass.hassapi as hass
 
 
 class BathroomVentFan(hass.Hass):
+    """Automatically turn on bathroom vent fan when humidity rises"""
+
     def initialize(self):
+        self.log("Starting up")
 
         self.fan = self.args["fan"]
         self.trend_sensor = self.args["trend_sensor"]
         self.humidity_sensor = self.args["humidity_sensor"]
 
-        self.timer_handle = None
+        self.max_timer_handle = None
+        self.min_timer_handle = None
+        self.on_min_time = False
         self.humidity = None
         self.fan_running = False
 
-        self.listen_state(self.turn_on, entity=self.trend_sensor, new="On")
+        # If the fan is turned on manually or the trend sensor goes on, start
+        # the fan
+        self.listen_state(self.turn_on, entity=self.fan, new="on")
+        self.listen_state(self.turn_on, entity=self.trend_sensor, new="on")
+
         self.listen_state(self.humidity_change, entity=self.humidity_sensor)
 
-    @attribute
+    @property
     def humidity_max(self):
         return self.humidity * 1.05
 
-    def turn_on(self):
+    def turn_on(self, entity, attribute, old, new, kwargs):
+        if not self.fan_running:
+            self.log("Turning on")
+            self.call_service("fan/turn_on", entity_id=self.fan)
+            self.fan_running = True
 
-        # Check if we are already running
-        if self.fan_running:
-            return
+            # Save the current humidity reading so we can run the fan until we are
+            # close to this again.
+            self.humidity = float(self.get_state(self.humidity_sensor))
+            self.log("Current humidity is at: {}".format(self.humidity))
+            self.log("Humidity threshold is at: {}".format(self.humidity_max))
 
-        self.call_serivce("fan/turn_on", entity_id=self.fan)
-        self.fan_running = True
+            # Set a timer to not run the fan for more than an hour
+            self.max_timer_handle = self.run_in(self.turn_off, 60 * 60)
+            # Set a timer to always run at least 10 minutes. Don't want the fan
+            # going on and off constantly.
+            self.on_min_time = False
+            self.min_timer_handle = self.run_in(self.hit_min_time, 60 * 10)
 
-        # Save the current humidity reading so we can run the fan until we are
-        # close to this again.
-        self.humidity = self.get_state(self.humidity_sensor)
+    def turn_off(self, kwargs):
+        if self.on_min_time and self.fan_running:
+            self.log("Turning off")
+            self.call_service("fan/turn_off", entity_id=self.fan)
 
-        # Set a timer to not run the fan for more than an hour
-        self.timer_handle = self.run_in(self.turn_off, 60 * 60)
+            # If we were turned off before the timer expired, cancel it
+            self.cancel_timer(self.max_timer_handle)
 
-    def turn_off(self):
-        self.call_service("fan/turn_off", entity_id=self.fan)
+            # Reset variables
+            self.max_timer_handle = None
+            self.humidity = None
+            self.fan_running = False
 
-        # If we were turned off before the timer expired, cancel it
-        self.cancel_timer(self.timer_handle)
+    def hit_min_time(self, kwargs):
+        self.log("On minimum time")
+        self.on_min_time = True
+        self.min_timer_handle = None
 
-        # Reset variables
-        self.timer_handle = None
-        self.humidity = None
-        self.fan_running = False
-
-    def humidity_change(self):
-        if self.fan_running:
-            if self.get_state(self.humidity_sensor) < self.humidity_max:
-                self.turn_off()
+    def humidity_change(self, entity, attribute, old, new, kwargs):
+        """When the humidity changes, check if it is within the threshold to
+        turn off."""
+        self.log("Detected humidity change")
+        if float(self.get_state(self.humidity_sensor)) < self.humidity_max:
+            self.turn_off(kwargs)
