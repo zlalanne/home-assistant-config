7d099b8d31e5c154a9139f9fa51b7b738c525e4f
Zack Lalanne <zack.lalanne@gmail.com>
2019-12-08 09:30:20 -0600

    Adding more robust checks to christmas tree lights

--- config/appdaemon/apps/christmas_tree_lights.py
+++ config/appdaemon/apps/christmas_tree_lights.py
@@ -3,6 +3,9 @@ import random
 import appdaemon.plugins.hass.hassapi as hass
 import sys
 
+MORNING_TIME = datetime.time(5, 0, 0)
+NIGHT_TIME = datetime.time(22, 0, 0)
+
 
 class ChristmasTreeLights(hass.Hass):
     """Automatically turn on Christmas Tree Lights based on presence"""
@@ -15,27 +18,31 @@ class ChristmasTreeLights(hass.Hass):
         self.randomize = self.args["randomize"]
 
         # Turn on if someone comes home
-        self.listen_state(self.turn_on, entitiy=self.people, new="home")
+        self.listen_state(self.turn_on, entitiy=self.people, new="home", old="not_home")
 
         # Turn off if everyone leaves home
-        self.listen_state(self.turn_off, entity=self.people, new="not_home")
+        self.listen_state(self.turn_off, entity=self.people, new="not_home", old="home")
 
-        # Turn on at 5 AM
-        morning_time = datetime.time(5, 0, 0)
-        self.run_once(self.turn_on_timer, morning_time)
+        self.run_once(self.turn_on_timer, MORNING_TIME)
 
-        # Turn off at 10 PM
-        night_time = datetime.time(22, 0, 0)
-        self.run_once(self.turn_off_timer, night_time)
+        self.run_once(self.turn_off_timer, NIGHT_TIME)
 
         # Initialize a timer handle for random effects
         self.random_timer_handle = None
 
+        # Should we turn on lights at startup
+        if self.get_state(self.people) == "home":
+            now = self.time()
+            if now < NIGHT_TIME and now > MORNING_TIME:
+                self.turn_on_lights()
+
     def turn_on(self, entity, attribute, old, new, kwargs):
+        self.log("Someone came home")
         self.turn_on_lights()
 
     def turn_on_timer(self, kwargs):
         # Make sure someone is home
+        self.log("Random light timer expired")
         if self.get_state(self.people) == "home":
             self.turn_on_lights()
 
@@ -67,6 +74,8 @@ class ChristmasTreeLights(hass.Hass):
             else:
                 self.call_service("light/turn_on", entity_id=self.lights, effect=effect)
             # Randomly change the effect in an hour
+            if self.random_timer_handle:
+                self.cancel_timer(self.random_timer_handle)
             self.random_timer_handle = self.run_in(self.turn_on_timer, 60 * 60)
         else:
             self.call_service(
@@ -83,4 +92,4 @@ class ChristmasTreeLights(hass.Hass):
         self.log("Turning off lights")
         self.call_service("light/turn_off", entity_id=self.lights)
         self.cancel_timer(self.random_timer_handle)
-        self.log("Random timer handle is now: {}".format(self.random_timer_handle))
+        self.random_timer_handle = None
