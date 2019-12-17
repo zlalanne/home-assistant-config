7c06d535ab7c3bf4442ac3f166dfbb6a16aebd51
Zack Lalanne <zack.lalanne@gmail.com>
2019-12-16 21:05:06 -0600

    Adding appdaemon app for toggling office lights when garage door opens

--- config/appdaemon/apps/home_notification.py
+++ config/appdaemon/apps/home_notification.py
@@ -1,14 +1,22 @@
+import appdaemon.plugins.hass.hassapi as hass
+
 
 class HomeNotification(hass.Hass):
-
     def initialize(self):
         self.log("Starting up")
 
         self.garage_door = self.args["garage_door"]
         self.lights = self.args["lights"]
 
+        self.flashcount = 0
+
+        self.listen_state(self.start_toggle, entity=self.garage_door, new="open")
+
+    def start_toggle(self, entity, attribute, old, new, kwargs):
+        self.run_in(self.flash, 1)
+
     def flash(self, kwargs):
         self.toggle(self.lights)
         self.flashcount += 1
-        if self.flashcount < 15:
+        if self.flashcount < 6:
             self.run_in(self.flash, 1)
