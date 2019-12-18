aeac8d90ae2047a4900044b240271c718f070aae
Zack Lalanne <zack.lalanne@gmail.com>
2019-12-18 11:20:57 -0600

    Fixing light toggling notification

--- config/appdaemon/apps/home_notification.py
+++ config/appdaemon/apps/home_notification.py
@@ -8,11 +8,11 @@ class HomeNotification(hass.Hass):
         self.garage_door = self.args["garage_door"]
         self.lights = self.args["lights"]
 
-        self.flashcount = 0
-
         self.listen_state(self.start_toggle, entity=self.garage_door, new="open")
 
     def start_toggle(self, entity, attribute, old, new, kwargs):
+        self.log("Garage door open, starting to toggle lights")
+        self.flashcount = 0
         self.run_in(self.flash, 1)
 
     def flash(self, kwargs):
